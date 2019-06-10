2006fa69c81abc57de53332f1d58a3ac677c8ddc
Исэль <33750773+Iseru-san@users.noreply.github.com>
2019-06-10 19:58:06 +0300

    Delete ports.h

--- include/yot/ports.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-
-void outb(unsigned short port, unsigned char value);
-void outw(unsigned short port, unsigned short value);
-unsigned char inb(unsigned short port);
-unsigned short inw(unsigned short port);
-
